244a3c2beec664a43cb84ba7fe1222edbf42d12b
mahmoudisma3il18 <mody.bg12@gmail.com>
2022-10-26 00:53:43 +0200

    Box it

--- Labs/Lab 1/Box It/main.cpp
+++ Labs/Lab 1/Box It/main.cpp
@@ -8,27 +8,24 @@
 using namespace std;
 
 class Box {
-
-    private :
+private :
     int l;
     int b;
     int h;
 
     public :
-    Box(){  //Constructor
-        l = 0;
-        b = 0;
-        h = 0;
+    Box(){
+        l=b=h=0;
     }
-    Box(int l,int b , int h){
-        this->l = l;
-        this->h = h;
-        this->b = b;
+    Box(int lenght,int breadth , int height){
+        l = lenght;
+        h = height;
+        b = breadth;
     }
-    Box(const Box &b){
-        this->l = b.l;
-        this->h = b.h;
-        this->b = b.b;
+    Box(const Box &box){
+        l = box.l;
+        h = box.h;
+        b = box.b;
     }
 
     int getLength(){
@@ -41,7 +38,7 @@ class Box {
         return h;
     }
     long long CalculateVolume(){
-        return (l*b*h);
+        return ((long long)l*b*h);
     }
    friend bool operator < (Box& b1, Box& b2)
         {
@@ -57,7 +54,6 @@ class Box {
             s << b1.l << " " << b1.b << " " << b1.h;
             return s;
         }
-
 };
 
 int main()
